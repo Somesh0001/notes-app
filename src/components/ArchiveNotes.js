22a34b72ea5c2dfdfece37bbbe522b64e63a5f71
Somesh Dwivedi <116994984+Somesh0001@users.noreply.github.com>
2023-10-27 15:12:01 +0530

    Fourth Commit , Archived section done

---
+++ src/components/ArchiveNotes.js
@@ -0,0 +1,60 @@
+import React from "react";
+import Note from "./Note";
+import UnarchiveIcon from "@mui/icons-material/Unarchive";
+import { useState } from "react";
+function ArchiveNote() {
+  const [isHover, setIsHover] = useState(false);
+  const text =
+    "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, at officiis neque reprehenderit distinctio illo pariatur placeat aspernatur tenetur deleniti expedita? Adipisci esse iure ea quae maiores explicabo alias. Fugiat ut est inventore, quibusdam dolor harum odio repudiandae assumenda accusantium!";
+  return (
+    <>
+     
+        
+        <div
+          style={{
+            
+            margin: "10px",
+            textAlign: "center",
+            height: "auto",
+            backgroundColor: "#fff",
+            marginTop: "1em",
+            display: "flex",
+            justifyContent: "left",
+            alignItems: "left",
+
+            position: "relative",
+          }}
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+        >
+          <div style={{ opacity: "0.6" }}>
+            {" "}
+            <Note text={text} />
+          </div>
+
+          <button
+            style={{
+              position: "relative",
+              right: "4em",
+              top: "4em",
+              color: "black",
+              maxHeight: "1em",
+              fontWeight: "bold   ",
+              fontSize: "2em",
+              border: "1px solid green",
+              borderRadius: "10px",
+              cursor: "pointer",
+              backgroundColor: "green",
+              opacity: isHover ? "1" : "0",
+            }}
+            onMouseEnter={() => setIsHover(true)}
+          >
+            <UnarchiveIcon />
+          </button>
+        </div>
+     
+    </>
+  );
+}
+
+export default ArchiveNote;
--- src/pages/Archived.js
+++ src/pages/Archived.js
@@ -1,9 +1,37 @@
-import React from 'react'
-
+import React from "react";
+import ArchiveNote from "../components/ArchiveNotes";
 function Archived() {
   return (
-    <div>Archived</div>
-  )
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        flexWrap: "wrap",
+        width: "100%",
+       
+      }}
+    >
+      <h1>Archieves</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          maxWidth: "80vw",
+          alignSelf: "center",
+          alignItems: "center",
+          justifyContent: "center",
+
+          boxSizing: "border-box",
+        }}
+      >
+        <ArchiveNote />
+        <ArchiveNote />
+      </div>
+    </div>
+  );
 }
 
-export default Archived
+export default Archived;
